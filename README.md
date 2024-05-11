9ce372ac5080c70bafcc2ce5de34749a345ce151
kang1870 <kang1870@naver.com>
2024-05-12 01:14:42 +0900

    병원 검색 페이지 추가

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # TeamProject
 2024.05.07 "React Build"
 
-2024 05 08 "React routes, Redux Build"
+2024.05.08 "React routes, Redux Build"
 
+2024.05.12 "공통 카테고리 컴포넌트, 병원 검색 페이지 디자인 추가"
--- teamproject/src/App.js
+++ teamproject/src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import Category from './routes/Category';
+import Category from './components/Category';
+import { Route, Router, Routes } from 'react-router-dom';
+import SearchHospital from './routes/SearchHospital';
+import SearchHospitalTitle from './routes/SearchHospitalTitle';
 
 function App() {
   return (
-    <div className="App">
-      <Category></Category>
-    </div>
+    <>
+      <Routes>
+        <Route path="/hospitals" element={<SearchHospital/>}></Route>
+        <Route path='/hospitals/:keyword' element={<SearchHospitalTitle/>}></Route>
+      </Routes>
+    </>
   );
 }
 
